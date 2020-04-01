8aee88befced821e25b25cf70ccf09aa0b9e8240
Andres <a.martinez@uniandes.edu.co>
2020-04-01 07:34:16 -0500

    Servidor carga video y hace broadcast

--- servidor.py
+++ servidor.py
@@ -1,5 +1,8 @@
 import socket
 import struct
+import cv2
+import numpy as np
+import time
 
 # Cositas varias
 IP     = "224.1.1.1"
@@ -11,9 +14,37 @@ Socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 # Que los mensajes vivan un segundo!
 Socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, struct.pack('b', 1))
 
+# Cargar video
+def cargar(ruta):
+    p=[]
+    Video = cv2.VideoCapture(ruta)
+    if (Video.isOpened()== False):
+        print("Error cargando el video")
+        return None
+    while(Video.isOpened()):
+        ret, frame = Video.read()
+        if ret == True:
+            p.append(frame)
+        else:
+            break
+    Video.release()
+    return p
+
+# Obtener video
+v=cargar('Video.mp4')
+
 # Informar
 print("Arranqué en la IP: "+IP+" y puerto: "+str(Puerto))
 
 # Transmitir
 while(True):
-    Socket.sendto(str.encode("Buenaaas"), (IP,Puerto))
+    for i in v:
+        # Un nuevo frame se enviara
+        Socket.sendto(str.encode("Nuevo Frame"), (IP,Puerto))
+        # Se envia el tamaño
+        Socket.sendto(np.array([np.size(i[0,:,0]),np.size(i[0,0,:]),np.size(i[:,0,0])]).tobytes(), (IP,Puerto))
+        # Se secciona el frame
+        for j in range(np.size(i[:,0,0])):
+            Socket.sendto(i[j,:,:].tobytes(), (IP,Puerto))
+        # Espera un momento
+        time.sleep(.01)
