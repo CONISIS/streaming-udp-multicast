0434e09e541c4cd9ee3ef8677268559389a8a352
Andres <a.martinez@uniandes.edu.co>
2020-04-06 18:43:30 -0500

    Servidor envia catalogo

--- servidor.py
+++ servidor.py
@@ -4,6 +4,22 @@ import cv2
 import numpy as np
 import time
 import threading
+from os import walk
+import argparse
+import sys
+from pynput import keyboard
+import select
+
+# Obtener argumentos
+parser = argparse.ArgumentParser()
+parser.add_argument('--ip', type=str, help='La IP donde el servidor escucha nuevos clientes', default="127.0.0.1")
+parser.add_argument('--puerto', type=int, help='El puerto donde el servidor escucha nuevos clientes', default=20001)
+args = parser.parse_args()
+
+# Variables de conexion con clientes
+IP = args.ip
+Puerto = args.puerto
+TamBuffer = 1024
 
 #Canales activos
 ca = []
@@ -25,8 +41,8 @@ def cargar(ruta):
     Video.release()
     return p
 
-
-def canal(IP = "224.1.1.1",Puerto = 20001,v=cargar('MoratS.mp4'),e=0):
+# Inicia un canal de broadcast
+def canal(IP = "224.1.1.1",Puerto = 20001,v=cargar('Video.mp4'),e=0):
     global estado
     # Crear socket
     with socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) as Socket:
@@ -35,7 +51,7 @@ def canal(IP = "224.1.1.1",Puerto = 20001,v=cargar('MoratS.mp4'),e=0):
         Socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, struct.pack('b', 1))
 
         # Informar
-        print("Arranqué en la IP: "+IP+" y puerto: "+str(Puerto))
+        print("Canal "+str(e)+": Arranqué en "+IP+":"+str(Puerto))
 
         # Transmitir
         while estado[e]:
@@ -51,14 +67,26 @@ def canal(IP = "224.1.1.1",Puerto = 20001,v=cargar('MoratS.mp4'),e=0):
                     # Enviar
                     Socket.sendto(enviar, (IP,Puerto))
 
-    print("Termine en la IP: "+IP+" y puerto: "+str(Puerto))
+    print("Canal "+str(e)+": Termine en "+IP+":"+str(Puerto))
+
+# Escuchar teclado para detectar interrupcion
+interrumpir = True
+def on_press(tecla):
+    global interrumpir
+    if tecla == keyboard.KeyCode.from_char("q"):
+        print ("\nApagando...")
+        interrumpir = False
+        return False
 
 
+print ("\nBienvenido al servidor de streaming por broadcast UDP (presione 'q' para salir)\n")
+
+# Cargar contenido
+print ("Cargando contenido...")
 v=cargar('Video.mp4')
 
-print("Iniciando canales")
+print("Iniciando canales...")
 #Inicia canal
-
 estado.append(True)
 t1 = threading.Thread(target = canal,kwargs={'IP':"224.1.1.1",'v':v,'e':1})
 t1.start()
@@ -70,10 +98,37 @@ estado.append(True)
 t2 = threading.Thread(target = canal,kwargs={'IP':"224.1.1.2",'v':v,'e':2})
 t2.start()
 ca.append(("224.1.1.2",t2))
-time.sleep(10)
-estado[1]=False
-time.sleep(10)
-estado[2]=False
 
-t1.join()
-t2.join()
+# Loop principal
+# Inicia escucha de teclado
+with keyboard.Listener(on_press=on_press) as listener:
+    # Crear socket
+    with socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) as Socket:
+        # Conecta socket a ip y puerto
+        Socket.bind((IP, Puerto))
+        # Iniciando escucha de clientes
+        print("Escuchando clientes en: "+IP+":"+str(Puerto)+"\n")
+        while interrumpir:
+            # Verifica si llego mensaje
+            leer, escribir, error = select.select([Socket],[],[],10)
+            # Si llego
+            if Socket in leer:
+                recibido = Socket.recvfrom(TamBuffer)
+                # Verifica mensaje
+                if recibido[0].decode() == "Hola":
+                    # Informa solicitud
+                    print("Solicitud de catalogo: "+recibido[1][0]+":"+str(recibido[1][1]))
+                    # Envia catalogo
+                    s = "Canales disponibles: "
+                    for i in ca:
+                        s=s+i[0]+" "
+                    Socket.sendto(str.encode(s), recibido[1])
+    listener.join()
+
+# Detener threads
+print("Apagando canales...")
+for i in range(len(estado)):
+    estado[i]=False
+for i in ca:
+    i[1].join()
+print()
